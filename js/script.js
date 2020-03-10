4af1c8f28896ccf8facae25f129f1b654c601532
alfavit-256 <61536310+alfavit-256@users.noreply.github.com>
2020-03-10 17:01:06 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,162 @@
+/*=========================================
+                Preloader
+============================================*/
+$(window).on('load', function () { // makes sure that whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+
+/*=========================================
+              Responsive Tabs
+============================================*/
+$(function () {
+	
+	$("#langs-tabs").responsiveTabs({
+		animation: 'slide',
+	});
+
+});
+
+/* =========================================
+              Stats
+============================================ */
+$(function () {
+
+    $(".stats-p").counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+});
+
+/* =========================================
+              Google Map
+============================================ */
+$(window).on('load', function () {
+
+    // Map Variables
+    var addressString = '230 Broadway, NY, New York 10007, USA';
+    var myLatlng = {
+        lat: 41.31185541,
+        lng: 69.27927017
+    };
+
+    // 1. Render Map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: myLatlng
+    });
+
+    // 2. Add Marker
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "Click To See Address"
+    });
+
+    // 3. Add Info Window
+    var infowindow = new google.maps.InfoWindow({
+        content: addressString
+    });
+
+    // Show info window when user clicks marker
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
+    // 4. Resize Function
+    google.maps.event.addDomListener(window, 'resize', function () {
+
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+
+    });
+
+});
+
+
+/* =========================================
+              Navigation
+============================================ */
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+
+            // Show back to top button
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+
+            // Hide back to top button
+            $("#back-to-top").fadeOut();
+        }
+    }
+});
+
+// Smooth Scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get section id like #about, #servcies, #work, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo");
+
+    });
+
+});
+
+/* =========================================
+              Mobile Menu
+============================================ */
+$(function () {
+
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function () {
+        $("#mobile-nav").css("height", "0%");
+    });
+
+});
+
+
+
+
+
+
